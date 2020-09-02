333e5ac495861563de9389e76994f7cfd7bc51be
Txing <yuxue0126@126.com>
2020-09-02 09:22:32 +0800

    JZOffer20

---
+++ JZOffer20.py
@@ -0,0 +1,139 @@
+import math
+from enum import Enum
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def stringToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]" # 最后一个逗号不输出
+
+
+class Solution(object):
+    def isNumber(self, s: str) -> bool:
+        State = Enum("State", [
+            "STATE_INITIAL",
+            "STATE_INT_SIGN",
+            "STATE_INTEGER",
+            "STATE_POINT",
+            "STATE_POINT_WITHOUT_INT",
+            "STATE_FRACTION",
+            "STATE_EXP",
+            "STATE_EXP_SIGN",
+            "STATE_EXP_NUMBER",
+            "STATE_END",
+        ])
+        Chartype = Enum("Chartype", [
+            "CHAR_NUMBER",
+            "CHAR_EXP",
+            "CHAR_POINT",
+            "CHAR_SIGN",
+            "CHAR_SPACE",
+            "CHAR_ILLEGAL",
+        ])
+
+        def toChartype(ch: str) -> Chartype:
+            if ch.isdigit(): # 检测字符串是否只由数字组成
+                return Chartype.CHAR_NUMBER
+            elif ch.lower() == "e": # 字符串转换大写为小写
+                return Chartype.CHAR_EXP
+            elif ch == ".":
+                return Chartype.CHAR_POINT
+            elif ch == "+" or ch == "-":
+                return Chartype.CHAR_SIGN
+            elif ch == " ":
+                return Chartype.CHAR_SPACE
+            else:
+                return Chartype.CHAR_ILLEGAL
+
+        transfer = {
+            State.STATE_INITIAL: {
+                Chartype.CHAR_SPACE: State.STATE_INITIAL,
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_POINT: State.STATE_POINT_WITHOUT_INT,
+                Chartype.CHAR_SIGN: State.STATE_INT_SIGN,
+            },
+            State.STATE_INT_SIGN: {
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_POINT: State.STATE_POINT_WITHOUT_INT,
+            },
+            State.STATE_INTEGER: {
+                Chartype.CHAR_NUMBER: State.STATE_INTEGER,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_POINT: State.STATE_POINT,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_POINT: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_POINT_WITHOUT_INT: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+            },
+            State.STATE_FRACTION: {
+                Chartype.CHAR_NUMBER: State.STATE_FRACTION,
+                Chartype.CHAR_EXP: State.STATE_EXP,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_EXP: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+                Chartype.CHAR_SIGN: State.STATE_EXP_SIGN,
+            },
+            State.STATE_EXP_SIGN: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+            },
+            State.STATE_EXP_NUMBER: {
+                Chartype.CHAR_NUMBER: State.STATE_EXP_NUMBER,
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+            State.STATE_END: {
+                Chartype.CHAR_SPACE: State.STATE_END,
+            },
+        }
+
+        st = State.STATE_INITIAL
+        for ch in s:
+            typ = toChartype(ch)
+            if typ not in transfer[st]:
+                return False
+            st = transfer[st][typ]
+
+        return st in [State.STATE_INTEGER, State.STATE_POINT, State.STATE_FRACTION, State.STATE_EXP_NUMBER,
+                      State.STATE_END]
+
+
+
+
+
+
+
+
+
+def main():
+    s = "0.."
+    solution=Solution().isNumber(s)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
+
