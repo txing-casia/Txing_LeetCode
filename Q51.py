5dc52ef5bf9a277a59a3a6c2965d2860df5c37f8
Txing <yuxue0126@126.com>
2020-09-03 09:46:11 +0800

    Q51

---
+++ Q51.py
@@ -0,0 +1,105 @@
+import math
+from enum import Enum
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def stringToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]" # 最后一个逗号不输出
+
+
+class Solution(object):
+    def solveNQueens1(self, n):
+        def generateBoard():
+            board = list()
+            for i in range(n):
+                row[queens[i]] = "Q"
+                board.append("".join(row))
+                row[queens[i]] = "."
+            return board
+
+        def backtrack(row: int):
+            if row == n:
+                board = generateBoard()
+                solutions.append(board)
+            else:
+                for i in range(n):
+                    if i in columns or row - i in diagonal1 or row + i in diagonal2:
+                        continue
+                    queens[row] = i
+                    columns.add(i)
+                    diagonal1.add(row - i)
+                    diagonal2.add(row + i)
+                    backtrack(row + 1)
+
+                    columns.remove(i)
+                    diagonal1.remove(row - i)
+                    diagonal2.remove(row + i)
+
+        solutions = list()
+        queens = [-1] * n
+        columns = set()
+        diagonal1 = set()
+        diagonal2 = set()
+        row = ["."] * n
+        backtrack(0)
+        return solutions
+
+
+    def solveNQueens2(self, n) :
+        def generateBoard():
+            board = list()
+            for i in range(n):
+                row[queens[i]] = "Q"
+                board.append("".join(row))
+                row[queens[i]] = "."
+            return board
+
+        def solve(row: int, columns: int, diagonals1: int, diagonals2: int):
+            if row == n:
+                board = generateBoard()
+                solutions.append(board)
+            else:
+                availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2))
+                while availablePositions:
+                    position = availablePositions & (-availablePositions)
+                    availablePositions = availablePositions & (availablePositions - 1)
+                    column = bin(position - 1).count("1")
+                    queens[row] = column
+                    solve(row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1)
+
+        solutions = list()
+        queens = [-1] * n
+        row = ["."] * n
+        solve(0, 0, 0, 0)
+        return solutions
+
+
+def main():
+    s = 6
+    solution=Solution().solveNQueens2(s)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
+
