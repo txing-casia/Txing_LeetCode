7f0519489f48bc0e723882c77289f43894557a36
Txing <yuxue0126@126.com>
2020-09-04 09:05:28 +0800

    Q257

---
+++ Q257.py
@@ -0,0 +1,79 @@
+import math
+from enum import Enum
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def stringToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]" # 最后一个逗号不输出
+
+
+class Solution(object):
+    def binaryTreePaths1(self, root):
+        def construct_paths(root, path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:  # 当前节点是叶子节点
+                    paths.append(path)  # 把路径加入到答案中
+                else:
+                    path += '->'  # 当前节点不是叶子节点，继续递归遍历
+                    construct_paths(root.left, path)
+                    construct_paths(root.right, path)
+
+        paths = []
+        construct_paths(root, '')
+        return paths
+
+
+    def binaryTreePaths2(self, root) :
+        paths = list()
+        if not root:
+            return paths
+
+        node_queue = collections.deque([root])
+        path_queue = collections.deque([str(root.val)])
+
+        while node_queue:
+            node = node_queue.popleft()
+            path = path_queue.popleft()
+
+            if not node.left and not node.right:
+                paths.append(path)
+            else:
+                if node.left:
+                    node_queue.append(node.left)
+                    path_queue.append(path + '->' + str(node.left.val))
+
+                if node.right:
+                    node_queue.append(node.right)
+                    path_queue.append(path + '->' + str(node.right.val))
+        return paths
+
+def main():
+    s = [1,2,3,'None',5]
+    solution=Solution().binaryTreePaths1(s)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
+
