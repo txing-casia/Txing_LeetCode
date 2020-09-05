b31ce8e289651d855fddfbe22f8c5e29a09d1474
Txing <yuxue0126@126.com>
2020-09-05 13:27:50 +0800

    Q60

---
+++ Q60.py
@@ -0,0 +1,65 @@
+import math
+from enum import Enum
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def stringToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]" # 最后一个逗号不输出
+
+###############################################################
+###############################################################
+###############################################################
+
+class Solution(object):
+    def getPermutation(self, n: int, k: int) -> str:
+        # generate arithmetic sequence
+        factorial = [1]
+        for i in range(1, n):
+            factorial.append(factorial[-1] * i)
+
+        k -= 1
+        ans = list()
+        valid = [1] * (n + 1)
+        for i in range(1, n + 1):
+            order = k // factorial[n - i] + 1
+            for j in range(1, n + 1):
+                order -= valid[j]
+                if order == 0:
+                    ans.append(str(j))
+                    valid[j] = 0
+                    break
+            k %= factorial[n - i]
+
+        return "".join(ans)
+
+
+def main():
+    n = 3
+    k = 2
+    solution=Solution().getPermutation(n,k)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
+
