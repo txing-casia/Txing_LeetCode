725bd011a4229e1401ca28d6a3754c01cac7f6e5
txing-casia <yuxue0126@126.com>
2021-03-15 09:56:06 +0800

    Create Q5.py

---
+++ Q5.py
@@ -0,0 +1,110 @@
+import math
+from enum import Enum
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def stringToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]" # 最后一个逗号不输出
+
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        # 枚举子串的长度 L+1
+        for L in range(n):
+            # 枚举子串的起始位置 i，这样可以通过 j=i+L 得到子串的结束位置
+            for i in range(n):
+                j = i + L
+                if j >= len(s):
+                    break
+                if L == 0:
+                    dp[i][j] = True
+                elif L == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+                if dp[i][j] and L + 1 > len(ans):
+                    ans = s[i:j+1]
+        return ans
+
+
+    def expandAroundCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+
+    def longestPalindrome1(self, s: str) -> str:
+        start, end = 0, 0
+        for i in range(len(s)):
+            left1, right1 = self.expandAroundCenter(s, i, i)
+            left2, right2 = self.expandAroundCenter(s, i, i + 1)
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+        return s[start: end + 1]
+
+
+    def expand(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return (right - left - 2) // 2
+
+    def longestPalindrome2(self, s: str) -> str:
+        end, start = -1, 0
+        s = '#' + '#'.join(list(s)) + '#'
+        arm_len = []
+        right = -1
+        j = -1
+        for i in range(len(s)):
+            if right >= i:
+                i_sym = 2 * j - i
+                min_arm_len = min(arm_len[i_sym], right - i)
+                cur_arm_len = self.expand(s, i - min_arm_len, i + min_arm_len)
+            else:
+                cur_arm_len = self.expand(s, i, i)
+            arm_len.append(cur_arm_len)
+            if i + cur_arm_len > right:
+                j = i
+                right = i + cur_arm_len
+            if 2 * cur_arm_len + 1 > end - start:
+                start = i - cur_arm_len
+                end = i + cur_arm_len
+        return s[start+1:end+1:2]
+
+
+def main():
+    s = "cbabd"
+    solution=Solution().longestPalindrome2(s)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
+
+
