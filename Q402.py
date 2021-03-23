975b2faeab2fdf2f77b8dcb6dbc6b067f0e8e217
txing-casia <yuxue0126@126.com>
2021-03-23 09:44:06 +0800

    Create Q402.py

---
+++ Q402.py
@@ -0,0 +1,66 @@
+import math
+from enum import Enum
+import bisect
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def stringToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]" # 最后一个逗号不输出
+
+
+
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        numStack = []
+        
+        # 构建单调递增的数字串
+        for digit in num:
+            while k and numStack and numStack[-1] > digit:
+                numStack.pop()
+                k -= 1
+        
+            numStack.append(digit)
+        
+        # 如果 K > 0，删除末尾的 K 个字符
+        finalStack = numStack[:-k] if k else numStack
+        
+        # 抹去前导零
+        # return "".join(finalStack)
+        return "".join(finalStack).lstrip('0') or "0"
+
+
+
+
+
+def main():
+    num = "140032219000"
+    k = 3
+    solution=Solution().removeKdigits(num,k)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
+
+
