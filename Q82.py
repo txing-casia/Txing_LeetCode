734f00a9acd24bcb45122f0b51b78b3049f7a070
txing-casia <yuxue0126@126.com>
2021-03-31 16:36:32 +0800

    Create Q82.py

---
+++ Q82.py
@@ -0,0 +1,59 @@
+import math
+from enum import Enum
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def stringToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]" # 最后一个逗号不输出
+
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        # 判断链表非空
+        if not head:
+            return head
+        # 头节点可能要修改，需要添加虚头节点
+        dummy = ListNode([])
+        dummy.next = head
+        cur = dummy
+        while cur.next and cur.next.next:
+            if cur.next.val == cur.next.next.val:
+                x = cur.next.val
+                while cur.next and cur.next.val == x:
+                    cur.next = cur.next.next
+            else:
+                cur = cur.next
+            print(listNodeToString(dummy.next))
+        return dummy.next
+
+
+
+def main():
+    head = stringToListNode([1,2,3,3,3,4,4,5])
+    solution=Solution().deleteDuplicates(head)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
